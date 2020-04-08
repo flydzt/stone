156997aeca20986f20ee7baf9956ba362c974717
flydzt <flydzt@gmail.com>
2020-04-08 22:50:39 +0800

    day4

---
+++ src/main/java/com/flydzt/stone/Name.java
@@ -0,0 +1,11 @@
+package com.flydzt.stone;
+
+public class Name extends ASTLeaf {
+    public Name(Token t) {
+        super(t);
+    }
+
+    public String name() {
+        return token().getText();
+    }
+}
---
+++ src/main/java/com/flydzt/stone/NumberLiteral.java
@@ -0,0 +1,11 @@
+package com.flydzt.stone;
+
+public class NumberLiteral extends ASTLeaf {
+    public NumberLiteral(Token t) {
+        super(t);
+    }
+
+    public int value() {
+        return token().getNumber();
+    }
+}
